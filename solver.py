4db1c22b0c276fd3a98b76150f5233d054b4a2a7
joaoabcoelho <joaoabcoelho@gmail.com>
2020-07-27 14:37:29 +0200

    Add greedy approach and make print more compact

--- solver.py
+++ solver.py
@@ -7,8 +7,9 @@ Item = namedtuple("Item", ['index', 'value', 'weight'])
 
 from DP import dynamic_prog
 from BB import branch_and_bound
+from Greedy import greedy_density
 
-def solve_it(n_shire=10, verbose=True):
+def solve_it(n_shire=10, verbose=True, greedy=False):
 
     capacity = int(n_shire * (n_shire + 5) / 2)
 
@@ -26,7 +27,10 @@ def solve_it(n_shire=10, verbose=True):
         print()
         print("DP size = {0}M".format(dpsize/1e6))
     
-    if dpsize < 20e6:
+    if greedy:
+        if verbose: print("Try greedy approach")
+        value, taken = greedy_density(capacity, items)
+    elif dpsize < 20e6:
         if verbose: print("Try dynamic programming")
         value, taken = dynamic_prog(capacity, items, verbose)
     else:
@@ -49,11 +53,25 @@ def solve_it(n_shire=10, verbose=True):
     for item in items:
         if not taken[item.index]:
             shires_won.append("{0}".format(item.index+1))
+    tmp = shires_won[:1]
+    last = tmp[0]
+    for s in shires_won:
+        if int(s)>int(last)+1:
+            if last != tmp[-1]:
+                tmp[-1] += '-'+last
+            tmp.append(s)
+        elif s==shires_won[-1]:
+            if last != tmp[-1]:
+                tmp[-1] += '-'+s
+            else: tmp.append(s) 
+        last = s
+    shires_won = tmp
     if verbose:
         print()
         print("Electoral votes won: {0} of {1} ({2:.1f}%)".format(elec_votes, elec_total, elec_perc))
         print("Popular votes won: {0} of {1} ({2:.1f}%)".format(votes, total, perc))
-        print("Shires won: " + ", ".join(shires_won[:-1]) + " and " + shires_won[-1])
+        if(len(shires_won)>1): print("Shires won: " + ", ".join(shires_won[:-1]) + " and " + shires_won[-1])
+        else:                  print("Shires won: " + shires_won[0])
 
     return perc
 
@@ -61,8 +79,12 @@ def solve_it(n_shire=10, verbose=True):
 if __name__ == '__main__':
 
     n_shire = 10
+    verbose = True
+    greedy = False
     if len(sys.argv) > 1:
         n_shire = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        greedy = int(sys.argv[2])
         
-    solve_it(n_shire)
+    solve_it(n_shire, verbose, greedy)
 
